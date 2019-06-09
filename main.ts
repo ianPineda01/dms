e8ce613241b979ab0f3ee2162a62add42279c0e1
ianPineda01 <lalo.pineda@comunidad.unam.mx>
2019-06-09 00:15:35 -0500

    First commit

---
+++ main.ts
@@ -0,0 +1,28 @@
+import * as electron from "electron";
+const {app, BrowserWindow} = electron;
+
+const createWindow = () =>{
+    let window = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences:{
+            nodeIntegration: true
+        }
+    });
+    window.loadFile("main.html");
+    window.webContents.openDevTools();
+    window.on("closed", () =>{
+        window = null;
+    });
+}
+app.on("ready", createWindow);
+app.on("window-all-closed", () =>{
+   if(process.platform !== "darwin"){
+       app.quit();
+   } 
+});
+app.on("activate", () =>{
+    if(window === null){
+        createWindow();
+    }
+});
