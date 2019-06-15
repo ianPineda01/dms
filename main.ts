fc9be4df45e18219e9a220e0455db4fb8d1c75d9
ianPineda01 <lalo.pineda@comunidad.unam.mx>
2019-06-14 20:18:17 -0500

    Drop file sends path to main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,80 @@
-import {app, BrowserWindow} from "electron";
+import {app, BrowserWindow, Menu, ipcMain, IpcMain, IpcMessageEvent} from "electron";
+import * as url from "url";
+import * as path from "path";
+import * as fs from "fs";
 
-const createWindow = () =>{
-    let window = new BrowserWindow({
+let mainWindow:BrowserWindow;
+const createMainWindow = () =>{
+    mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences:{
             nodeIntegration: true
         }
     });
-    window.loadFile("main.html");
-    window.webContents.openDevTools();
-    window.on("closed", () =>{
-        window = null;
+    mainWindow.loadFile("main.html");
+    mainWindow.webContents.openDevTools();
+    mainWindow.on("closed", () =>{
+        app.quit();
     });
+    Menu.setApplicationMenu(Menu.buildFromTemplate([
+        {
+            label:"Archivo",
+            submenu:[
+                {
+                    label: "Añadir"
+                },
+                {
+                    label: "Editar"
+                },
+                {
+                    label: "Eliminar"
+                },
+                {
+                    label: "Salir",
+                    accelerator: process.platform === "darwin" ? "Command+Q": "Ctrl+Q",
+                    click(){
+                        app.quit();
+                    }
+                }
+            ]
+        },
+        {
+            label: "Ventas"
+        },
+        {
+            label: "Base de datos"
+        },
+        {
+            label: "Cotizaciones"
+        },
+        {
+            label: "Compras"
+        },
+        {
+            label: "Clientes"
+        },
+        {
+            label: "Facturas"
+        }
+    ]));
 }
-app.on("ready", createWindow);
+
+app.on("ready", createMainWindow);
 app.on("window-all-closed", () =>{
    if(process.platform !== "darwin"){
        app.quit();
    } 
 });
+
 app.on("activate", () =>{
-    if(window === null){
-        createWindow();
+    if(mainWindow === null){
+        createMainWindow();
     }
+});
+
+ipcMain.on("fileDropped", (e, path)=>{
+    const data = fs.readFileSync(path,"utf-8");
+    console.log(path);
+    console.log(data);
 });
--- mainRenderer.ts
+++ mainRenderer.ts
@@ -1,3 +1,4 @@
+const {ipcRenderer} = require("electron");
 interface dbEntry{
     quantity:number
     code:string
@@ -9,6 +10,11 @@ const createCell: (input:string) => HTMLTableDataCellElement = (input) =>{
     td.appendChild(document.createTextNode(input));
     return td;
 }
+const createHeader: (input:string) => HTMLTableHeaderCellElement = (input) =>{
+    const th = document.createElement("th");
+    th.appendChild(document.createTextNode(input));
+    return th;
+}
 const createRow: (input:dbEntry) => HTMLTableRowElement = (input) =>{
     const tr = document.createElement("tr");
     tr.appendChild(createCell(input.quantity.toString()));
@@ -17,18 +23,26 @@ const createRow: (input:dbEntry) => HTMLTableRowElement = (input) =>{
     tr.appendChild(createCell(input.timeStamp.toString()));
     return tr;
 }
+const createTableHeader: () => HTMLTableRowElement = () =>{
+    const tr = document.createElement("tr");
+    tr.appendChild(createHeader("Cantidad"));
+    tr.appendChild(createHeader("Codigo"));
+    tr.appendChild(createHeader("Precio"));
+    tr.appendChild(createHeader("timeStamp"));
+    return tr;
+}
 const createTable: (db:dbEntry[]) => HTMLTableElement = (db) =>{
     const table = document.createElement("table");
+    table.appendChild(createTableHeader());
     db.map((value) =>{
         table.appendChild(createRow(value));
     });
     return table;
 }
-document.body.appendChild(createTable([
-    {
-        quantity:1,
-        code:"Pro",
-        price:200,
-        timeStamp:0
-    }
-]));
+document.addEventListener("dragover", (e)=> {
+    e.preventDefault();
+});
+document.addEventListener("drop", (e)=> {
+    e.preventDefault();
+    ipcRenderer.send("fileDropped", e.dataTransfer.files[0].path);
+});
